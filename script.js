684f1ec35b2045074c41b562aa96ad0aaf655890
wkwyatt <wkwyatt1@gmail.com>
2015-11-16 12:05:04 -0500

    initial commit
    more angular practice

---
+++ script.js
@@ -0,0 +1,10 @@
+function Houses (name, img, address, price, Zestimate, disc, mortgage) {
+	
+	this.name = name;
+	this.img = img;
+	this.address = address;
+	this.price = price;
+	this.Zestimate = Zestimate;
+	this.disc = disc;
+	this.mortgage = mortgage;
+}
